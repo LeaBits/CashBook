841f04a4c244670b1183b8bf77eda78650548b0c
Leanne de Goeij <lr.degoey@gmail.com>
2021-01-26 13:54:00 +0100

    Mobile version

--- src/Controller/DefaultCrudController.php
+++ src/Controller/DefaultCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use Mobile_Detect;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,11 +13,21 @@ class DefaultCrudController extends AbstractCrudController
 {
     protected $pageSize;
     protected $sort;
+    protected $detect;
 
     public function __construct()
     {
         $this->pageSize = 100;
         $this->sort = ['title' => 'ASC'];
+        $this->detect = new Mobile_Detect();
+    }
+
+    protected function isOnMobile(): bool
+    {
+        if ($this->detect->isMobile() && !$this->detect->isTablet()) {
+            return true;
+        }
+        return false;
     }
 
     public static function getEntityFqcn(): string
--- src/Controller/TransactionCrudController.php
+++ src/Controller/TransactionCrudController.php
@@ -14,7 +14,6 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
-use Symfony\Bundle\MakerBundle\Doctrine\RelationOneToMany;
 
 class TransactionCrudController extends DefaultCrudController
 {
@@ -32,30 +31,39 @@ class TransactionCrudController extends DefaultCrudController
 
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id')
-                ->hideOnForm()
-                ->hideOnIndex(),
-            AssociationField::new('bankAccount', 'Bank Account')
-                ->setTemplatePath('admin/bankAccountField.html.twig')
-                ->setSortable(false),
-            BooleanField::new('isOff', 'Taken off')
-                ->setSortable(false)
-                ->renderAsSwitch(false),
-            MoneyField::new('amount', 'Amount')->setCurrency('EUR'),
-            DateField::new('date', 'Date')
-                ->setFormat('d/M/y'),
-            AssociationField::new('transactionCategory', 'Category')
-                ->setTemplatePath('admin/bankAccountField.html.twig')
-                ->setSortable(false),
-            AssociationField::new('transactionSubcategory', 'Subcategory')
-                ->setSortable(false),
-            AssociationField::new('repaymentTransaction', 'Repaid by transaction')
-                ->setSortable(false),
-            TextEditorField::new('comments', 'Comments')
-                ->setSortable(false)
-                ->hideOnIndex(),
-        ];
+        yield IdField::new('id')
+            ->hideOnForm()
+            ->hideOnIndex();
+
+        $account = AssociationField::new('bankAccount', 'Bank Account');
+        if($this->isOnMobile()){ $account->hideOnIndex(); }
+        yield $account->setTemplatePath('admin/bankAccountField.html.twig')
+            ->setSortable(false);
+
+        $isOff = BooleanField::new('isOff', 'Taken off');
+        if($this->isOnMobile()){ $isOff->hideOnIndex(); }
+        yield $isOff->setSortable(false)
+            ->renderAsSwitch(false);
+
+        yield MoneyField::new('amount', '€')
+            ->setCurrency('EUR');
+        yield DateField::new('date', 'Date')
+            ->setFormat('d/M/y');
+        yield AssociationField::new('transactionCategory', 'Category')
+            ->setTemplatePath('admin/bankAccountField.html.twig')
+            ->setSortable(false);
+
+        $subCategory = AssociationField::new('transactionSubcategory', 'Subcategory');
+        if($this->isOnMobile()){ $subCategory->hideOnIndex(); }
+        yield $subCategory->setSortable(false);
+
+        $repayment = AssociationField::new('repaymentTransaction', 'Repaid by transaction');
+        if($this->isOnMobile()){ $repayment->hideOnIndex(); }
+        yield $repayment->setSortable(false);
+
+        yield TextEditorField::new('comments', 'Comments')
+            ->setSortable(false)
+            ->hideOnIndex();
     }
 
     public function configureFilters(Filters $filters): Filters
